9c9be82c21952289d54711f0692739f14b61c005
Derek Caldwell <djohncaldwell@gmail.com>
2023-07-07 16:06:25 -0700

    navigation added to home page

---
+++ public/js/home.js
@@ -0,0 +1,14 @@
+// Navigates to bench page
+document.querySelector(".bench-btn").addEventListener("click", () => {
+    window.location.href = "/bench";
+})
+
+// Navigates to squat page
+document.querySelector(".squat-btn").addEventListener("click", () => {
+    window.location.href = "/squat";
+})
+
+// Navigates to deadlift page
+document.querySelector(".deadlift-btn").addEventListener("click", () => {
+    window.location.href = "/deadlift";
+})
