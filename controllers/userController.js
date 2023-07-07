d27ac6d4c756b9b17de01dcd2a56d79dbaa367c8
Derek Caldwell <djohncaldwell@gmail.com>
2023-07-07 11:51:12 -0700

    signup page created, new user route added, new user func to sign up page

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,4 +16,27 @@ router.get("/", async (req, res)=> {
     }
 })
 
+router.post("/create", async (req, res) => {
+    try {
+        const newUser = await User.create({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            username: req.body.username,
+            password: req.body.password
+        });
+        
+        if (newUser){
+            req.session.userId = newUser.id;
+            req.session.username = newUser.username;
+            res.status(200).json(newUser);
+        } else {
+            res.status(404).json( {msg: "Something went wrong! "});
+        }
+    } catch (error){
+        console.log(error);
+        res.status(500).json({ msg: "An error has occurred!" });
+    }
+})
+
 module.exports = router;
---
+++ public/js/signup.js
@@ -0,0 +1,32 @@
+// Creates new user on click
+document.querySelector(".submit-btn").addEventListener("click", async (e) => {
+    // Displays a window alert if an input is left blank.
+    for (let input of document.querySelectorAll("input")){
+        if (!input.value){
+            return window.alert("Please enter a value for all fields.");
+        }
+    }
+
+    // API call to create new user with input values
+    const newUser = await fetch("/api/users/create", {
+        method: "POST",
+        body: JSON.stringify({
+            email: document.querySelector("#email").value,
+            firstName: document.querySelector("#first-name").value,
+            lastName: document.querySelector("#last-name").value,
+            username: document.querySelector("#username").value,
+            password: document.querySelector("#password").value
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    // Alerts whether user was created
+    if (newUser.ok){
+        window.alert("User successfully created!");
+    } else {
+        window.alert("User not created!");
+    }
+
+})
