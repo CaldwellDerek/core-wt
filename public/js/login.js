9b291b2c33053fb1d85586ee71b3696481b2523f
Derek Caldwell <djohncaldwell@gmail.com>
2023-07-07 18:29:34 -0700

    further styling and form function

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,4 @@
-// Validates input, logs user in
+// Validates input, logs user in on click
 document.querySelector(".submit-btn").addEventListener("click", async () => {
     for (let input of document.querySelectorAll("input")){
         if (!input.value){
@@ -21,5 +21,33 @@ document.querySelector(".submit-btn").addEventListener("click", async () => {
         window.location.href = "/home";
     } else {
         window.alert("Oops!")
+    }
+})
+
+// Validates input, logs user in on "Enter"
+document.addEventListener("keypress", async (e) => {
+    if (e.key == "Enter"){
+        for (let input of document.querySelectorAll("input")){
+            if (!input.value){
+                return window.alert("Please enter a value for all fields.");
+            }
+        }
+    
+        const logUser = await fetch("/api/users/login", {
+            method: "POST",
+            body: JSON.stringify({
+                username: document.querySelector("#username").value,
+                password: document.querySelector("#password").value
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    
+        if (logUser.ok){
+            window.location.href = "/home";
+        } else {
+            window.alert("Oops!")
+        }
     }
 })
