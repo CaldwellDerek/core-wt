2f599a7de2862209eea8543864900cce5b616c26
Derek Caldwell <djohncaldwell@gmail.com>
2023-07-10 11:42:15 -0700

    all bench maxes route added

--- controllers/benchMaxController.js
+++ controllers/benchMaxController.js
@@ -23,6 +23,20 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/all", async (req, res) => {
+    try {
+        const allBenchMaxes = await BenchMax.findAll();
+        if (allBenchMaxes){
+            return res.status(200).json(allBenchMaxes);
+        } else {
+            return res.status(404).json({ msg: "Unable to find Bench Maxes!" });
+        }
+    } catch (error){
+        console.log(error);
+        return res.status(500).json({ msg: "An error has occurred!" });
+    }
+});
+
 router.post("/create", async (req, res) => {
     try {
         const newMax = await BenchMax.create({
