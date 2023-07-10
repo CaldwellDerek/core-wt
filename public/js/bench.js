4bc0eedded83f189cc292eed2e7d103cdb632f55
Derek Caldwell <djohncaldwell@gmail.com>
2023-07-10 13:13:47 -0700

    modal added to confirm new one rep max

--- public/js/bench.js
+++ public/js/bench.js
@@ -32,7 +32,15 @@ document.querySelector(".submit-btn").addEventListener("click", async () => {
         }
     }
 
-    // API call to create new bench max"
+    document.querySelector(".weight-span").textContent = document.querySelector("#weight").value;
+    document.querySelector(".reps-span").textContent = document.querySelector("#reps").value;
+    document.querySelector(".new-max").textContent = Math.round((100 * document.querySelector("#weight").value) / (101.3 - (2.67123 * document.querySelector("#reps").value)));
+
+    const modal = new bootstrap.Modal(document.querySelector("#modal"), {});
+    modal.show();
+})
+
+document.querySelector(".modal-confirm").addEventListener("click", async () => {
     const createMax = await fetch("/api/bench/create", {
         method: "POST",
         body: JSON.stringify({
@@ -43,19 +51,9 @@ document.querySelector(".submit-btn").addEventListener("click", async () => {
         }
     });
 
-    // Alerts whether max was created
-    if (createMax.ok){
-        window.alert("Bench Max successfully created!");
-    } else {
-        window.alert("Bench Max not created!");
-    }
-
-    // Clears input field values
-    for (let input of document.querySelectorAll("input")){
-        input.value = "";
-    }
+    // for (let input of document.querySelectorAll("input")){
+    //     input.value = "";
+    // }
 
     location.reload();
-
 })
-
